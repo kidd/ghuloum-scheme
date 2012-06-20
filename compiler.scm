e1d1714271dbf72b4baf596fbc2104822dea6cec
Raimon Grau <raimonster@gmail.com>
2012-06-20 02:14:17 +0200

    null, #t and #f implemented

--- compiler.scm
+++ compiler.scm
@@ -1,14 +1,39 @@
 (load "tests/tests-driver.scm")
-(load "tests/tests-1.1-req.scm")
+;(load "tests/tests-1.1-req.scm")
+(load "tests/tests-1.2-req.scm")
 
-;;; fixed integers
+;;; immediates
+
+(define fxshift 2)
+(define fxmask #x03)
+(define bool_f #x2F)
+(define bool_t #x6F)
+(define wordsize 4) ;byte
+
+(define fixnum-bits (- (* wordsize 8) fxshift))
+(define fxlower (- (expt 2 (- fixnum-bits 1))))	  ;min fixnum
+(define fxupper (- (expt 2 (- fixnum-bits 1)) 1)) ;max fixnum
+
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fxlower x fxupper)))
+
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
+(define (immediate-rep x)
+  (cond
+   ((fixnum? x) (ash x fxshift))
+   ((boolean? x) (if x bool_t bool_f))
+   ((char? x) (logior (ash x 8) #b1111))
+   ((null? x) #b00111111)))
+
 (define (compile-program x port)
-  (unless (integer? x) (error 'interror "not an integer"))
+  (unless (immediate? x) (error 'immediaterror "not an immediate"))
   (emit port ".text")
   (emit port ".globl scheme_entry")
   (emit port ".type scheme_entry, @function")
   (emit port "scheme_entry:")
-  (emit port "movl $~s, %eax" x)
+  (emit port "movl $~s, %eax" (immediate-rep x))
   (emit port "ret"))
 
 (test-all)
--- startup.c
+++ startup.c
@@ -2,6 +2,7 @@
 /* define all scheme constants */
 #define bool_f               0x2F
 #define bool_t               0x6F
+#define null                 0b00111111
 #define fx_mask              0x03
 #define fx_tag               0x00
 #define fx_shift                2
@@ -14,6 +15,10 @@ static void print_ptr(ptr x){
     printf("%d", ((int)x) >> fx_shift);
   } else if(x == bool_f){
     printf("#f");
+  } else if(x == bool_t){
+    printf("#t");
+  } else if(x == null){
+    printf("()");
   } else {
     printf("#<unknown 0x%08x>", x);
   }
@@ -22,7 +27,6 @@ static void print_ptr(ptr x){
 
 
 int main(int argc, char** argv){
-  printf("%d\n", scheme_entry());
-  /* print_ptr(scheme_entry()); */
+  print_ptr(scheme_entry());
   return 0;
 }
